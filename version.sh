345459eb3b0567ead6b121456bae2bac3467ffe4
Markus Hahn <mhahn@mailbox.org>
2022-08-19 08:50:44 -0700

    version updater updated

--- version.sh
+++ version.sh
@@ -10,12 +10,11 @@ fi
 PRODUCT=badpeggy
 VERSION=$1
 
+FNAME=pom.xml
+perl -i -0 -pe "s/(<artifactId>badpeggy<\/artifactId>\s*<version>).*(<\/version>)/\${1}$VERSION\${2}/g" $FNAME
+
 FNAME=src/main/java/com/coderslagoon/badpeggy/GUI.java
 perl -i -0 -pe "s/(final\ static\ String\ VERSION\ = ).*/\${1}\"$VERSION\";/g" $FNAME
-
-FNAME=MANIFEST.MF
-perl -i -0 -pe "s/(Specification-Version: *).*/\${1}$VERSION/g" $FNAME
-perl -i -0 -pe "s/(Implementation-Version: *).*/\${1}$VERSION/g" $FNAME
 
 FNAME=etc/Info.plist
 perl -i -0 -pe "s/(<key>CFBundleShortVersionString<\/key>\s*<string>).*(<\/string>)/\${1}$VERSION\${2}/g" $FNAME
